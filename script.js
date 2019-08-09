59567990426f6e11bee7b0fadde84ad9925f5be2
Jennifer <jennico985@gmail.com>
2019-08-08 21:49:18 -0400

    Add existing file

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ $(document).ready(function() {
      $('.fade').each( function(i){
 
          var bottom_of_object = $(this).position().top + $(this).outerHeight();
+         var middle_of_object = $(this).position().top + $(this).outerHeight()/2;
          var bottom_of_window = $(window).scrollTop() + $(window).height();
 
          /* If the object is completely visible in the window, fade it it */
-         if( bottom_of_window > bottom_of_object ){
+         if( bottom_of_window > middle_of_object ){
 
              $(this).animate({'opacity':'1'},800);
 
