78ea78290ec16baafc8cd779ab6cc905f9f54889
Jennifer <jennico985@gmail.com>
2019-08-29 19:33:28 -0400

    styled for responsive

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
   /* Every time the window is scrolled ... */
   $(window).scroll( function(){
 
+    var nav = $('.navigation')
+    var sticky = nav.offsetTop;
+    if(window.pageYOffset >= sticky){
+      nav.addClass("sticky");
+    }else{
+      nav.removeClass("sticky");
+    }
+
      /* Check the location of each desired element */
      $('.fade').each( function(i){
 
@@ -17,8 +25,36 @@ $(document).ready(function() {
 
          }
 
-     });
+     });  /*fade-in*/
 
+  }); /*window scroll*/
+
+//Variables for pagespeed
+var mobileNav = $('.nav-items')
+var rhombus = $('.rhombus')
+
+  //Mobile hamburger click
+  $('.mobile-menu').click(function(){
+
+    //toggle mobile nav and rhombuses
+    if((mobileNav).hasClass('mobile-nav')){
+      mobileNav.removeClass('mobile-nav');
+      rhombus.css("display", "inline");
+    }else{
+        mobileNav.addClass('mobile-nav');
+        rhombus.css("display", "none");
+
+        //Close nav when the user makes a selection
+        $('a').click(function(){
+          mobileNav.removeClass('mobile-nav');
+          rhombus.css("display", "inline");
+        });
+    }
   });
+
+  /*Nav needs:
+    X  1. Close nav when the user clicks on a nav item
+      2. Sticky nav
+  */
 
 })
